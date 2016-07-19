28a775dccebb5f2febdf200f3a05e90880e2145d
userk <balestrieri.gepp@gmail.com>
2016-07-19 18:32:22 +0200

    Added script for reading owl file using rdflib and two A and T box

---
+++ aidi/owltest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+from rdflib.namespace import DC, FOAF
+
+g = Graph()
+#g.parse("http://dbpedia.org/resource/Elvis_Presley")
+g.parse("semantic_map1.owl")
+print len(g)
+
+
+for subj, pred, obj in g:
+	print("\n New object\n")
+	print((subj,pred,obj))
+ 	if (subj, pred, obj) not in g:
+		raise Exception("It better be!")
+
+print("--- printing mboxes ---")
+for person in g.subjects(RDF.type, FOAF.Person):
+    for mbox in g.objects(person, FOAF.mbox):
+        print(mbox)
+
+
+s = g.serialize(format='n3')
+print s
