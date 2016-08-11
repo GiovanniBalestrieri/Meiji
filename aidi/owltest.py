40a1986bb13cc2a204ce7343fb44c58bc1c1d299
userk <balestrieri.gepp@gmail.com>
2016-08-11 17:46:48 +0200

    Updated owlTest Still useless

--- aidi/owltest.py
+++ aidi/owltest.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
-from rdflib.namespace import DC, FOAF
+from rdflib.namespace import DC, FOAF, RDF
 
 g = Graph()
 #g.parse("http://dbpedia.org/resource/Elvis_Presley")
-#g.parse("semantic_map1.owl")
+g.parse("semantic_map2.owl")
 
-g.parse("test.owl")
+#g.parse("test.owl")
 print len(g)
 
+chair = URIRef("http://www.semanticweb.org/ontologies/2016/1/semantic_mapping_domain_model#Chair")
+if (chair,RDF.type,None) in g:
+	print("Chair found")
 
 for subj, pred, obj in g:
-	print("\n New object\n")
-	print((subj,pred,obj))
+	#print("\n New object\n")
+	#print((subj,pred,obj))
  	if (subj, pred, obj) not in g:
-		raise Exception("It better be!")
+		raise Exception("Iterator / Container Protocols are Broken!!")
 
-print("--- printing mboxes ---")
+#print("--- printing mboxes ---")
 for person in g.subjects(RDF.type, FOAF.Person):
     for mbox in g.objects(person, FOAF.mbox):
-        print(mbox)
+        #print(mbox)
+	pass
 
 
-s = g.serialize(format='n3')
-print s
+#s = g.serialize(format='n3')
+#print s
